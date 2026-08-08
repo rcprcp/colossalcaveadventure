b6d21183c29201be544317cf9697137579303d05
Bob Plotts <rcprcp@users.noreply.github.com>
2026-08-08 19:18:43 -0400

    compat_mod: use 64-bit kinds consistently; add BUG handler; make RAN/VOCAB 64-bit; use ishft for masks

--- advent_f2023.f90
+++ advent_f2023.f90
@@ -1,6 +1,6 @@
-! Advent Fortran2023 scaffolding
+! Advent Fortran2023 scaffolding (updated)
 module compat_mod
-  use iso_fortran_env, only: int64
+  use iso_fortran_env, only: int64, real64
   implicit none
   integer, parameter :: i64 = int64
 contains
@@ -20,76 +20,84 @@ contains
     if (n < 0 .or. n >= bit_size(cond)) then
       res = .false.
     else
-      mask = shiftl(1_i64, n)
+      ! use ishft to form mask (portable)
+      mask = ishft(1_i64, n)
       res = iand(cond, mask) /= 0_i64
     end if
   end function bitset64
 
-  ! Random integer in [0,n-1]
-  integer function RAN(n)
-    integer, intent(in) :: n
-    real :: r
+  ! Random integer in [0,n-1] returned as 64-bit integer
+  integer(kind=i64) function RAN(n)
+    integer(kind=i64), intent(in) :: n
+    real(kind=real64) :: r
     call random_number(r)
-    if (n <= 0) then
-      RAN = 0
+    if (n <= 0_i64) then
+      RAN = 0_i64
     else
-      RAN = int(r * real(n))
+      RAN = int(r * real(n, kind=real64), kind=i64)
     end if
   end function RAN
 
-  ! Minimal VOCAB stub used during scaffolding. Returns 0 for unknown.
-  integer function VOCAB(a,b)
-    integer, intent(in) :: a,b
-    VOCAB = 0
+  ! Minimal VOCAB stub used during scaffolding. Returns 0 for unknown (64-bit)
+  integer(kind=i64) function VOCAB(a,b)
+    integer(kind=i64), intent(in) :: a,b
+    VOCAB = 0_i64
   end function VOCAB
+
+  ! Simple BUG handler: print message and stop (keeps original interface minimal)
+  subroutine BUG(code)
+    integer(kind=i64), intent(in) :: code
+    print '(A,I0)', 'BUG called with code ', code
+    stop 'BUG'
+  end subroutine BUG
 
   ! Placeholder stubs for external procedures used by the original program.
   subroutine RSPEAK(i)
-    integer, intent(in) :: i
+    integer(kind=i64), intent(in) :: i
     ! placeholder: speak a message index
   end subroutine RSPEAK
 
   subroutine SPEAK(idx)
-    integer, intent(in) :: idx
+    integer(kind=i64), intent(in) :: idx
     ! placeholder
   end subroutine SPEAK
 
   subroutine PSPEAK(obj, kk)
-    integer, intent(in) :: obj, kk
+    integer(kind=i64), intent(in) :: obj, kk
     ! placeholder
   end subroutine PSPEAK
 
   subroutine DROP(obj, loc)
-    integer, intent(in) :: obj, loc
+    integer(kind=i64), intent(in) :: obj, loc
     ! placeholder
   end subroutine DROP
 
   subroutine MOVE(obj, loc)
-    integer, intent(in) :: obj, loc
+    integer(kind=i64), intent(in) :: obj, loc
     ! placeholder
   end subroutine MOVE
 
   subroutine CARRY(obj, loc)
-    integer, intent(in) :: obj, loc
+    integer(kind=i64), intent(in) :: obj, loc
     ! placeholder
   end subroutine CARRY
 
   subroutine GETIN(w1, w1x, w2, w2x)
     character(len=*), intent(out) :: w1
-    integer, intent(out) :: w1x, w2x
+    integer(kind=i64), intent(out) :: w1x, w2x
     character(len=*), intent(out) :: w2
     ! minimal placeholder; return empty words
     if (len(w1) > 0) w1 = ''
     if (len(w2) > 0) w2 = ''
-    w1x = 0; w2x = 0
+    w1x = 0_i64; w2x = 0_i64
   end subroutine GETIN
 
   subroutine A5TOA1(a5, a5x, filler, tk, k)
     character(len=*), intent(in) :: a5
-    integer, intent(in) :: a5x
+    integer(kind=i64), intent(in) :: a5x
     character(len=*), intent(in) :: filler
     character(len=*), intent(out) :: tk
-    integer, intent(in) :: k
+    integer(kind=i64), intent(in) :: k
     ! placeholder
     if (len(tk) > 0) tk = ''
   end subroutine A5TOA1
@@ -107,7 +115,6 @@ end module compat_mod
 program advent_scaffold
   use compat_mod
   implicit none
-  print *, 'Fortran2023 scaffolding for advent: compat_mod added.'
-  print *, 'This is stage 1: compatibility module and placeholder stubs.  '
-  print *, 'Next stage will convert advent.for into a full Fortran 2023 program using 64-bit integers.'
+  print *, 'Fortran2023 scaffolding for advent: compat_mod updated.'
+  print *, 'Stage 1 scaffolding improved: RAN/VOCAB/BUG use 64-bit kinds.'
 end program advent_scaffold
